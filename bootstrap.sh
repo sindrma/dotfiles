986df613193f35527d6e63d5959cc69e67f04886
Sindre Magnussen <sindrma@gmail.com>
2021-08-01 01:32:06 +0200

    feat: Add basic setup

---
+++ bootstrap.sh
@@ -0,0 +1,70 @@
+#==============
+# Install all the packages
+#==============
+sudo chown -R $(whoami):admin /usr/local
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+export PATH=/opt/homebrew/bin:$PATH
+brew doctor
+brew update
+
+#==============
+# Remove old dot flies
+#==============
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.gitignore > /dev/null 2>&1
+sudo rm -rf ~/.config > /dev/null 2>&1
+sudo rm -rf ~/Brewfile > /dev/null 2>&1
+
+#==============
+# Create symlinks in the home folder
+# Allow overriding with files of matching names in the custom-configs dir
+#==============
+#SYMLINKS+=('.vim')
+#ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+#SYMLINKS+=('.vimrc')
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+SYMLINKS+=('.zshrc')
+ln -sf ~/.dotfiles/homebrew/Brewfile ~/Brewfile
+SYMLINKS+=('Brewfile')
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+SYMLINKS+=('.gitconfig')
+ln -s ~/.dotfiles/git/.gitignore ~/.gitignore
+SYMLINKS+=('.gitignore')
+
+echo ${SYMLINKS[@]}
+
+cd ~
+brew bundle
+cd -
+
+#==============
+# Set zsh as the default shell
+#==============
+chsh -s /bin/zsh
+
+#==============
+# Install oh-my-zsh
+#==============
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#==============
+# Initialise Vundle plugins
+#==============
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+ 
+#==============
+# Spaceship theme
+#==============
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+
+#==============
+# And we are done
+#==============
+echo -e "\n====== All Done!! ======\n"
+echo
---
+++ vim/.vimrc
@@ -0,0 +1,44 @@
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" alternatively, pass a path where Vundle should install plugins
+"call vundle#begin('~/some/path/here')
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'terryma/vim-multiple-cursors'
+Plugin 'itchyny/lightline.vim'
+Plugin 'tpope/vim-surround'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'junegunn/fzf.vim'
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+" 
+" From command line: vim +PluginInstall +qall
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+
+au BufNewFile,BufFilePre,BufRead *.md set filetype=markdown
+syntax on
+set number
+set laststatus=2
+nnoremap <A-j> :m+<CR>==
+nnoremap <A-k> :m-2<CR>==
+inoremap <A-j> <Esc>:m+<CR>==gi
+inoremap <A-k> <Esc>:m-2<CR>==gi
+vnoremap <A-j> :m'>+<CR>gv=gv
+vnoremap <A-k> :m-2<CR>gv=gv
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
